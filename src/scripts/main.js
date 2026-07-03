06d43d62088b588f871ab61685aa12644ca012ae
student1232354 <tarasromanenko33@gmail.com>
2026-07-03 14:16:09 +0300

    first update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const dataitems = document.querySelectorAll('ul li');
+const ul = document.querySelector('ul');
+
+
+function compareSalary(a, b) {
+  const salarya = Number(a.dataset.salary.replace('$', '').replaceAll(',', ''));
+  const salaryb = Number(b.dataset.salary.replace('$', '').replaceAll(',', ''));
+
+  return salaryb - salarya;
+}
+function moveToEnd(li) {
+  ul.append(li);
+}
+
+
+const elementsArray = Array.from(dataitems);
+const sortingitems = elementsArray.sort(compareSalary);
+
+sortingitems.forEach(moveToEnd);
